00b8ff04607b3bae833a4cefe9aac1a4d9d77cc4
Chibueze Momah <michaelmomah05@gmail.com>
2021-04-10 20:15:20 -0400

    Final ICE

---
+++ Server/Config/db.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://localhost/webd6201";
+export const RemoteURI = "mongodb+srv://chibueze:O2uNMc8ERrb3fhka@webd6201.8i4n5.mongodb.net/webd6201?retryWrites=true&w=majority";
+export const Secret = "someSecret";
+export const Host = "MongoDB Atlas";
